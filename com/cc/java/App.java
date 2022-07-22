5071b3eb90bd9fefdc3437e18e7970e27daec583
NedosypI <irinanedosyp@gmx.de>
2022-07-22 18:21:07 +0200

    Erklärungen und Erweiterungen mit Tom

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,11 +5,17 @@ public class App {
     public static void main(String[] args) {
      
         // die 10000 ist wichtig wegen def. von Konstruktor
-    Konto konto1 = new Konto(10000);    
+    Konto konto1 = new Konto(10000, 3000);    
     System.out.println("Anfang: Konto1: " + konto1.getKontostand() + " €");
 
-    Konto konto2 = new Konto(10000);    
-    System.out.println("Anfang: Konto2: " + konto2.getKontostand() + " €");
+    Konto konto2 = new Konto();  
+    konto2.setUeberkredit(1500000);
+    // konto2.setKontostand(30000000);
+    System.out.println("+++++++++++++++++++++++++");
+
+    System.out.println("Anfang: Konto2: " + konto2.returnKontostand(30000000) + " €");
+
+    System.out.println("+++++++++++++++++++++++++");
 
     Konto konto3 = new Konto(10000);    
     System.out.println("Anfang: Konto3: " + konto3.getKontostand() + " €");
@@ -21,10 +27,15 @@ public class App {
     konto3.setKontostand(konto3.getKontostand() * 10);
 
 
+
     System.out.println("Aktuell: Konto1: " + konto1.getKontostand() + " €");
     System.out.println("Aktuell: Konto2: " + konto2.getKontostand() + " €");
     System.out.println("Aktuell: Konto3: " + konto3.getKontostand() + " €");
 
+    System.out.println("-------------------------------");
+
+    System.out.println("Überkredit: " + konto1.getUeberkredit());
+System.out.println(konto1.returnKontostand(15000));
 }
 
 
--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -3,22 +3,55 @@ package com.cc.java;
 public class Konto {
 
     private int kontostand;
+    private int ueberkredit;
+
     
     //Konstruktor
+    public Konto() {
+    }
+
     public Konto(int kontostand) {
         this.kontostand = kontostand;
     }
 
+
+// public Konto(double ueberkredit) {
+//         this.ueberkredit = ueberkredit;
+//     }
+
+public Konto(int kontostand, int ueberkredit) {
+        this.kontostand = kontostand;
+        this.ueberkredit = ueberkredit;
+    }
+
+
+    //Getter /Setter
     public int getKontostand() {
         return kontostand;
+    }
+
+    public int getUeberkredit() {
+        return ueberkredit;
     }
 
     public void setKontostand(int kontostand) {
         this.kontostand = kontostand;
     }
 
-    
-    
+    public void setUeberkredit(int ueberkredit) {
+        this.ueberkredit = ueberkredit;
     }
+
+    // Kombination: int returnKontostand => holen
+    // int kontostand => übergeben
+public int returnKontostand(int kontostand){
+    return kontostand + ueberkredit;
+}
+
+
+        
+    }
+    
+    
     
 
